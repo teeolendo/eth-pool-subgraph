0a9bc0a8cbdf7a91cec300d5017d3c64d3493121
Tony Olendo <tony.olendo@gmail.com>
2021-12-31 00:13:00 -0400

    First Commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -8,63 +8,91 @@ import {
   UniqueMaxUpdated,
   WeekAdvanced
 } from "../generated/EthPool/EthPool"
-import { ExampleEntity } from "../generated/schema"
+import {
+  DepositReceivedEntity,
+  RewardsReceivedEntity,
+  FundsWithdrawnEntity,
+  OwnershipTransferredEntity,
+  WeekAdvancedEntity,
+  UniqueMaxUpdatedEntity
+} from "../generated/schema"
 
 export function handleDepositReceived(event: DepositReceived): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = DepositReceivedEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new DepositReceivedEntity(event.transaction.from.toHex())
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
   entity.depositor = event.params.depositor
   entity.amount = event.params.amount
+  entity.currentWeek = event.params.currentWeek
 
-  // Entities can be written to the store with `.save()`
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.currentWeek(...)
-  // - contract.deposits(...)
-  // - contract.depositsPerWeek(...)
-  // - contract.maxNoOfUniqueWeeks(...)
-  // - contract.owner(...)
-  // - contract.showUniqueWeeklyDeposits(...)
-  // - contract.totalRewardsPerWeek(...)
-  // - contract.totalUniqueWeeklyDeposits(...)
 }
 
-export function handleFundsWithdrawn(event: FundsWithdrawn): void {}
+export function handleFundsWithdrawn(event: FundsWithdrawn): void {
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+  let entity = FundsWithdrawnEntity.load(event.transaction.from.toHex())
 
-export function handleRewardsReceived(event: RewardsReceived): void {}
+  if (!entity) {
+    entity = new FundsWithdrawnEntity(event.transaction.from.toHex())
+  }
 
-export function handleUniqueMaxUpdated(event: UniqueMaxUpdated): void {}
+  entity.rewarder = event.params.rewarder
+  entity.amount = event.params.amount
+  entity.currentWeek = event.params.currentWeek
 
-export function handleWeekAdvanced(event: WeekAdvanced): void {}
+  entity.save()
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  
+  let entity = OwnershipTransferredEntity.load(event.transaction.from.toHex())
+  
+  if (!entity) {
+    entity = new OwnershipTransferredEntity(event.transaction.from.toHex())
+  }
+
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.save()
+}
+
+export function handleRewardsReceived(event: RewardsReceived): void {
+  let entity = RewardsReceivedEntity.load(event.transaction.from.toHex())
+  
+  if (!entity) {
+    entity = new RewardsReceivedEntity(event.transaction.from.toHex())
+  }
+
+  entity.rewarder = event.params.rewarder
+  entity.amount = event.params.amount
+
+  entity.save()
+}
+
+export function handleUniqueMaxUpdated(event: UniqueMaxUpdated): void {
+  let entity = UniqueMaxUpdatedEntity.load(event.transaction.from.toHex())
+  
+  if (!entity) {
+    entity = new UniqueMaxUpdatedEntity(event.transaction.from.toHex())
+  }
+
+  entity.newMax = event.params.newMax
+
+  entity.save()
+}
+
+export function handleWeekAdvanced(event: WeekAdvanced): void {
+  let entity = WeekAdvancedEntity.load(event.transaction.from.toHex())
+  
+  if (!entity) {
+    entity = new WeekAdvancedEntity(event.transaction.from.toHex())
+  }
+
+  entity.newWeek = event.params.newWeek
+
+  entity.save()
+}
